ef43a3a219555327628e751e11e27fd04dfa4d08
Sina <sina@utmuser6-208.wireless.utoronto.ca>
2020-02-02 19:14:57 -0500

    fixed some errors in blockchain file

--- deercoin/blockchain.js
+++ deercoin/blockchain.js
@@ -1,7 +1,21 @@
+//https://www.npmjs.com/package/js-sha256
+var sha256 = require('js-sha256');
+
+class Block{
+  constructor(index, hash, previousHash, timestamp, data){
+      this.index = index;
+      this.previousHash = previousHash;
+      this.timestamp  = timestamp;
+      this.data = data;
+      this.hash = hash;
+  }
+}
+
 class Blockchain {
-    constructor(brand) {
+    constructor() {
       this.chain = [];
       this.genesisBlock = new Block(0 , '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7', null, 1465154705, 'my genesis block!!');
+      this.chain.push(this.genesisBlock);
     }
 
     /*
@@ -12,6 +26,25 @@ class Blockchain {
     }
 
     /*
+    *adds new block to the chain
+    */
+    addBlockToChain(block){
+      this.chain.push(block);
+    }
+
+    printChain(){
+      for(var i = 0;i<this.chain.length;i++){
+        console.log(this.chain[i]);
+        if(i != this.chain.length-1){
+          console.log("                |");
+          console.log("                |");
+          console.log("                |");
+          console.log("                v");
+        }
+      }
+    }
+
+    /*
     * returns the last block in the current chain
     */
     getLatestBlock(){
@@ -19,28 +52,29 @@ class Blockchain {
     }
 
     generateNextBlock(blockData){
-      previousBlock = getLatestBlock();
-      nextIndex = previousBlock.index + 1;
-      nextTimestamp = new Date().getTime() / 1000;
-      nextHahs = this.calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData);
-      newBlock = new Block(nextIndex, nextHash, previousBlock.hash, nextTimestamp, blockData);
+      var previousBlock = this.getLatestBlock();
+      var nextIndex = previousBlock.index + 1;
+      var nextTimestamp = new Date().getTime() / 1000;
+      var nextHash = this.calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData);
+      var newBlock = new Block(nextIndex, nextHash, previousBlock.hash, nextTimestamp, blockData);
+      this.addBlockToChain(newBlock);
     }
 
-    /*
+    /* NOT TESTED
     * returns the correct hash of new block(pending to be added to the chain)
     */
     calculateHashForBlock(newBlock){
-      previousBlock = getLatestBlock();
-      index = previousBlock.index + 1;
-      timestamp = newBlock.timestamp;
-      previousHash = previousBlock.hash;
-      blockData = newBlock.data;
+      var previousBlock = this.getLatestBlock();
+      var index = previousBlock.index + 1;
+      var timestamp = newBlock.timestamp;
+      var previousHash = previousBlock.hash;
+      var blockData = newBlock.data;
 
       return this.calculateHash(index, previousHash, timestamp, blockData);
     }
 
 
-    /*
+    /* NOT TESTED
     * checks the validity of a block
     * For a block to be valid the following must apply:
     *     The index of the block must be one number larger than the previous
@@ -62,3 +96,6 @@ class Blockchain {
 
     }
   }
+
+  module.exports.blockchain = Blockchain;
+  module.exports.block = Block;
