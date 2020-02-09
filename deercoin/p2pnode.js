161afd9bb80f8315c58ed4a2eaf6ef49f1e2b474
cell96 <anees.bajwa@mail.utoronto.ca>
2020-02-08 21:08:56 -0500

    worked on http server

--- deercoin/p2pnode.js
+++ deercoin/p2pnode.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const WebSocket = require("ws");
+import uuidv4 from 'uuid/v4';
 
 // Ports
 const http_port = 8001;
@@ -10,15 +11,54 @@ const p2p_port = 8002;
 // Array to store our connections
 let sockets = [];
 
+// Temp storage for peers. {unique id, peer}
+let map = new Map();
+
 // HTTP API
-let initialHTTPServer = () => {
+const initialHTTPServer = () => {
 
     // Set up JSON parsing
     const app = express();
     app.use(bodyParser.json());
 
+    // Default route
+    app.get('/', (req, res) => {
+        console.log("hello world");
+    });
+
     // Get the blockchain here
     app.get('/blockchain', (req, res) => {
-        // I'm too fucking tired for this
+        console.log("get blockchain");
     })
+
+    app.get('peers', (req, res) => {
+        console.log("get peers");
+        peerArray = Array.from(map.values());
+        res.send(peerArray);
+    });
+
+    app.post('addPeer', (req, res) => {
+        // uuidv4 generrates a unique identifier
+        const id = uuidv4();
+        const peer = req.body.peer;
+        map.set(id, peer);
+
+        const message = {
+            id,
+            peer,
+        };
+
+        message[id] = id;
+        message[peer] = peer;
+
+        res.send(message);
+    });
+
+    app.post('mine', (req, res) => {
+        console.log("mine block");
+    });
+
+    app.listen(process.env.PORT, () =>
+        console.log(`Example app listening on port ${process.env.PORT}!`),
+    );
 }
