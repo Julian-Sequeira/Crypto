a5a7dc2cc78efc650c7323538d06d3e3812d57a2
Julian <julian.sequeira@mail.utoronto.ca>
2020-02-18 18:02:24 -0500

    Added methods to sign and verify transaction strings

---
+++ src/cli-wallet/public-crypto.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+/**
+ * Generate public/private keys
+ */ 
+
+function genkeys() {
+    console.log("Generating a public/private key pair");
+    crypto.generateKeyPair('rsa', {
+        modulusLength: 4096,
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'pem'
+        },
+        privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'pem',
+            cipher: 'aes-256-cbc',
+            passphrase: 'deercoin'    // to encrypt the private key with
+        }
+    
+    // Write both of the generated keys to a file    
+    }, (err, publicKey, privateKey) => {
+        if (err) throw err; 
+    
+        // Write the public key to a file (fd = file descriptor)
+        fs.open('pubkey.pem', 'w', (err, fd) => {
+            if (err) throw err;
+            fs.write(fd, publicKey, (err, written, string) => { if (err) throw err; })
+            fs.close(fd, (err) => { if (err) throw err; })
+        })
+    
+        // Write the private key to a file 
+        // Should be fine if done asynchronously since they aren't logically dependent
+        fs.open('privkey.pem', 'w', (err, fd) => {
+            if (err) throw err;
+            fs.write(fd, privateKey, (err, written, string) => { if (err) throw err; })
+            fs.close(fd, (err) => { if (err) throw err; }) 
+        })
+    });
+}
+
+
+/**
+ * Sign a string with the private key 
+ * How it works- hash the transaction (we'll use SHA256)
+ * Then encrypt the hash (let Crypto.js worry about the encryption function)
+ */
+
+function signTransaction(transaction) {
+    let signature = "";
+    
+    // Load the private key from file
+    let privateKey = fs.readFile('privkey.pem', (err, key) => {
+        if (err) throw err;
+
+        // Using Crypto's sign object to make the signature
+        const sign = crypto.createSign('SHA256');
+        sign.write(transaction);
+        sign.end();
+        signature = sign.sign(privateKey, 'hex');
+    })
+    
+    return signature;
+}
+
+
+
+/**
+ * Verify a transaction was signed properly using a public key
+ * Take the transaction, hash it using SHA256
+ * Take the signature, decrypt it using the public key
+ * Compare the two strings for equality
+ */
+
+function verifyTransaction(transaction, signature, publicKey) {
+    const verify = crypto.createVerify('SHA256');
+    verify.write(transaction);
+    verify.end();
+    return verify.verify(publicKey, signature, 'hex');
+}
+
+
+
+
+
+module.exports = { genkeys, signTransaction, verifyTransaction };
