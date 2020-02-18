21d0fc170bc087fc6c6b71beacfbf1fe813bd428
Anees <bajwa.anees@gmail.com>
2020-02-17 19:10:02 -0500

    added react native app

---
+++ src/mobile/wallet-app/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Login from "./components/Login";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Login></Login>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
