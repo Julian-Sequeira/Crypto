2915c29e90cc28303742f7d9568142eda84887a3
Sina <sina@utmuser5-229.wireless.utoronto.ca>
2020-02-22 18:44:05 -0500

    finished the new format of blockchain

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ var initialPeers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 //TODO: calculate hash for block
 
 class Block {
-    constructor(index, previousHash, timestamp, data, hash, difficulty, work) {
+    constructor(index, previousHash, timestamp, data, hash, difficulty, work, nonce) {
         this.work = work; //stores the total work of the branch upto the current block
         this.difficulty = difficulty;
         this.index = index;
@@ -18,7 +18,7 @@ class Block {
         this.timestamp = timestamp;
         this.data = data;
         this.hash = hash.toString();
-        this.nonce = 0;
+        this.nonce = nonce; //helps us to get a hash with a desired difficulty
     }
 }
 
@@ -30,15 +30,13 @@ var MessageType = {
 };
 
 var getGenesisBlock = () => {
-    return new Block(0, "0", 1465154705, "my genesis block!!", "016534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7", 1, 1);
+    return new Block(0, "0", 1465154705, "my genesis block!!", "016534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7", 1, 1, -1);
 };
 
-var blockchain = [getGenesisBlock()];
-
 //our blockchain is more like a tree. It will keep track of all the children
-var blockchain2 = new Object();
-blockchain2['genesis'] = getGenesisBlock();
-blockchain2[(getGenesisBlock()).hash] = [(getGenesisBlock())];
+var blockchain = new Object();
+blockchain['genesis'] = getGenesisBlock();
+blockchain[(getGenesisBlock()).hash] = [(getGenesisBlock())];
 
 var longest = (getGenesisBlock());//stores the leaf node of the longest chain
 
@@ -46,7 +44,7 @@ var initHttpServer = () => {
     var app = express();
     app.use(bodyParser.json());
 
-    app.get('/blocks', (req, res) => res.send(JSON.stringify(blockchain2)));
+    app.get('/blocks', (req, res) => res.send(JSON.stringify(blockchain)));
     app.post('/mineBlock', (req, res) => {
         var newBlock = generateNextBlock(req.body.data);
         addBlock(newBlock);
@@ -106,49 +104,75 @@ var initErrorHandler = (ws) => {
     ws.on('error', () => closeConnection(ws));
 };
 
-
+/* 
+Given the blockData, it will create a new block and add it to the longest branch
+*/
 var generateNextBlock = (blockData) => {
     var previousBlock = getLatestBlock();
     var nextIndex = previousBlock.index + 1;
     var nextDifficulty = previousBlock.difficulty + 1;
     var nextWork = previousBlock.work + nextDifficulty;
     var nextTimestamp = new Date().getTime() / 1000;
-    var nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData);
-    return new Block(nextIndex, previousBlock.hash, nextTimestamp, blockData, nextHash, nextDifficulty, nextWork);
+    var nextNonce = getNonceForDifficultry(nextIndex, previousBlock.hash, nextTimestamp, blockData, nextDifficulty, nextWork);
+    var nextHash = calculateHash(nextIndex, previousBlock.hash, nextTimestamp, blockData, nextDifficulty, nextWork ,nextNonce);
+    return new Block(nextIndex, previousBlock.hash, nextTimestamp, blockData, nextHash, nextDifficulty, nextWork, nextNonce);
 };
 
-
-var calculateHashForBlock = (block) => {
+/*
+Returns a nonce value that can give us a hash with certain difficulty
+ */
+var getNonceForDifficultry = (index, previousHash, timestamp, data, difficulty, work) => {
     var newHash = null;
-
+    var nonce = -1;
     do{
-        block.nonce += 1;
-        newHash = calculateHash(block.index, block.previousHash, block.timestamp, block.data, block.difficulty, block.work, block.nonce);
-    } while (!checkHashFormat(newHash, block.difficulty));
-    
-    return newHash;
+        nonce += 1;//adds one to the nonce everytime the hash difficulty is not correct
+        newHash = calculateHash(index, previousHash, timestamp, data, difficulty, work, nonce);
+    } while (!checkHashFormat(newHash, difficulty));//checks hash difficulty
+    return nonce;
+}
+
+
+/*
+returns a hash for the given block given its attributes
+This function is used for check the hash validity of a block
+*/
+var calculateHashForBlock = (block) => {
+    return calculateHash(block.index, block.previousHash, block.timestamp, block.data, block.difficulty, block.work, block.nonce);
 };
 
+/*
+Checks if the hash has certain difficulty
+By difficulty, we mean the number of zeroes at the beginning of the hash
+For example, a hash with difficulty 2 must have a format 00xxxxxxxxx 
+*/
 var checkHashFormat = (hash, difficulty) => {
-    var regex = RegExp(`[0]{${difficulty}}.*`,"g");
+    var regex = RegExp(`^[0]{${difficulty}}.+`,"i");
     return regex.test(hash);
 };
 
 
+/*
+calculates hash of a block given all its attributes
+*/
 var calculateHash = (index, previousHash, timestamp, data, difficulty, work, nonce) => {
     return CryptoJS.SHA256(index + previousHash + timestamp + data + difficulty + work + nonce).toString();
 };
 
+/*
+checks if a block has more work than the current longest
+*/
 var checkMostWork = (newBlock) => {
     return longest.work < newBlock.work;
 }
 
+/*
+adds the block to the longest branch of the blockchain
+*/
 var addBlock = (newBlock) => {
     if (isValidNewBlock(newBlock, getLatestBlock())) {
-        if(newBlock.previousHash in blockchain2){//check to see if this branch exist at all
-            newBlock.work += newBlock.work + blockchain2[newBlock.previousHash][0].difficulty;
-            blockchain2[newBlock.previousHash].push(newBlock);
-            blockchain2[newBlock.hash] = [newBlock];
+        if(newBlock.previousHash in blockchain){//check to see if this branch exist at all
+            blockchain[newBlock.previousHash].push(newBlock);
+            blockchain[newBlock.hash] = [newBlock];
             //check to see if the new block is the most work done branch of blockchain
             //if so, assign that as the longest
             if (checkMostWork(newBlock)){
@@ -158,20 +182,23 @@ var addBlock = (newBlock) => {
     }
 };
 
+/*
+checks to see if the new block is valid
+*/
 var isValidNewBlock = (newBlock, previousBlock) => {
-    if (previousBlock.index + 1 !== newBlock.index) {
+    if (previousBlock.index + 1 !== newBlock.index) {//checks if newBlock's index is one more than the previous
         console.log('invalid index');
         return false;
-    }else if(previousBlock.difficulty + 1 !== newBlock.difficulty){
+    }else if(previousBlock.difficulty + 1 !== newBlock.difficulty){//checks if newBlock's difficulty is one more than the previous
         console.log('invalid difficulty');
         return false;
-    }else if(previousBlock.work + newBlock.difficulty !== newBlock.work){
+    }else if(previousBlock.work + newBlock.difficulty !== newBlock.work){//checks if newBlock's work makes sense
         console.log('invalid work');
         return false;
-    }else if (previousBlock.hash !== newBlock.previousHash) {
+    }else if (previousBlock.hash !== newBlock.previousHash) {//checks if newBlock's previous hash is the previousBlock's hash
         console.log('invalid previoushash');
         return false;
-    } else if (calculateHashForBlock(newBlock) !== newBlock.hash) {
+    } else if (calculateHashForBlock(newBlock) !== newBlock.hash) {//checks if newBlock has correct hash
         console.log(typeof (newBlock.hash) + ' ' + typeof calculateHashForBlock(newBlock));
         console.log('invalid hash: ' + calculateHashForBlock(newBlock) + ' ' + newBlock.hash);
         return false;
@@ -211,6 +238,9 @@ var handleBlockchainResponse = (message) => {
     }
 };
 
+/*
+replaces the blockchain with the one that contains more work and its valid
+*/
 var replaceChain = (newBlocks) => {
     if (isValidChain(newBlocks) && HasMoreWork(newBlocks)) {
         console.log('Received blockchain is valid. Replacing current blockchain with received blockchain');
@@ -221,6 +251,10 @@ var replaceChain = (newBlocks) => {
     }
 };
 
+/*
+checks the validity of any chain
+it mainly loops through all branches and uses isValidNewBlock to test its validity
+*/
 var isValidChain = (blockchainToValidate) => {
     if (JSON.stringify(blockchainToValidate['genesis']) !== JSON.stringify(getGenesisBlock())) {
         return false;
@@ -229,10 +263,10 @@ var isValidChain = (blockchainToValidate) => {
     var toCheck = [getGenesisBlock()];
     while (toCheck.length > 0){//loop through every branch
         var blockToCheck = toCheck[0];
-        var ChildrenList = blockchain2[blockToCheck.hash];
+        var ChildrenList = blockchain[blockToCheck.hash];
         for(var i = 1;i<ChildrenList.length; i++){//loop through everychild
             if (isValidNewBlock(ChildrenList[i], blockToCheck)) {
-                toCheck.push(ChildrenList[i]);
+                toCheck.push(ChildrenList[i]);//if valid, add the branch to list to further check
             } else {
                 return false;
             }
@@ -244,11 +278,17 @@ var isValidChain = (blockchainToValidate) => {
 
 };
 
+/*
+checks if the given blockchain has more work than the one we already have
+*/
 var HasMoreWork = (blockchain) => {
     var suggestedThickestBranch = findThickestBranch(blockchain);
     return suggestedThickestBranch.work > longest.work;
 }
 
+/*
+find the branch(leaf node) that contains the most work in the given blockchain
+*/
 var findThickestBranch = (blockchain) => {
     var thickestBranch = blockchain['genesis'];
     var toCheck = [thickestBranch];
@@ -267,7 +307,6 @@ var findThickestBranch = (blockchain) => {
     return thickestBranch;
 }
 
-//var getLatestBlock = () => blockchain[blockchain.length - 1];
 var getLatestBlock = () => longest;
 var queryChainLengthMsg = () => ({'type': MessageType.QUERY_LATEST});
 var queryAllMsg = () => ({'type': MessageType.QUERY_ALL});
@@ -287,5 +326,11 @@ initHttpServer();
 initP2PServer();
 
 //----------------------------------------------testing area
-/* */
-console.log(blockchain2);
+/*
+var newBlock = generateNextBlock('sina was here');
+addBlock(newBlock);
+newBlock = generateNextBlock('sina was here 2');
+addBlock(newBlock);
+console.log("-------------------");
+console.log(findThickestBranch(blockchain));
+*/
