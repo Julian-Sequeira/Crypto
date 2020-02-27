6e90118a0306eac900e45a0abd1bcc029e1c9d23
Julian <julian.sequeira@mail.utoronto.ca>
2020-02-27 16:03:04 -0500

    Moved miner code to the src directory

---
+++ src/miner/miner.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const credential = require('./credential');
+const block = require('./block');
+
+const {
+  publicKey,
+  // privateKey,
+} = credential.loadKeyPair();
+
+const blockTemplate = block.getBlockTemplate(publicKey);
+block.mineBlock(blockTemplate)
+  .then((newBlock) => {
+    // TODO: send the newly generated block to P2P nodes
+    // console.log(newBlock);
+  })
+  .catch((err) => {
+    console.log('error when mining block', err);
+  });
+
+// express middleware
+app.use(express.json());
+
+// Use Routes
+// app.use('/api/users/', require('./routes/api/users'));
+
+const port = process.env.PORT || 8378;
+
+app.listen(port, () => console.log(`Miner started on port ${port}`));
