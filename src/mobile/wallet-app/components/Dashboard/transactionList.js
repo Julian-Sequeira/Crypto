e02940ce77d7fc6e8cfe04baa15550a377c4e6aa
Anees <bajwa.anees@gmail.com>
2020-03-11 21:28:15 -0400

    UI complete, refresh list front-end done

--- src/mobile/wallet-app/components/Dashboard/transactionList.js
+++ src/mobile/wallet-app/components/Dashboard/transactionList.js
@@ -1,32 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View, Modal, TouchableOpacity, Alert, TextInput } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 import Data from './data.js';
-import { FlatList, TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import Constants from 'expo-constants';
-import { List, ListItem } from 'react-native-elements';
-
-// TODO: try this https://shoutem.github.io/docs/ui-toolkit/components/list-view, https://shoutem.github.io/docs/ui-toolkit/introduction
-
-const Item = ({ title }) => {
-    return (
-        <TouchableOpacity
-            // onPress={() => onSelect(id)}
-            style={styles.item}
-        >
-            <Text style={styles.title}>{title}</Text>
-        </TouchableOpacity>
-    );
-}
+import { ListItem } from 'react-native-elements';
 
 export default class TransactionList extends React.Component {
 
     state = {
         data: Data,
+        refreshing: false,
     }
 
     componentDidMount = () => {
+        this.requestTransactionHistory();
+    }
+
+    handleRefresh = () => {
+        this.setState( { refreshing: true } );
+        console.log("refreshed")
+        this.requestTransactionHistory();
+    }
+
+    requestTransactionHistory = () => {
+        this.setState( { refreshing: false } );
         // TODO: Fetch data
     }
 
@@ -46,7 +43,7 @@ export default class TransactionList extends React.Component {
     // TODO: add list refresh
 
     render() {
-        const { data } = this.state;
+        const { data, refreshing } = this.state;
 
         return (
             <View style={styles.container}>
@@ -55,13 +52,13 @@ export default class TransactionList extends React.Component {
                     renderItem={({ item }) => (
                         <ListItem
                             title={item.title}
-                            //containerStyle={{ borderBottomWidth: 0 }}
                             bottomDivider
-                            // contentContainerStyle={styles.item}
                         />
                     )}
                     keyExtractor={item => item.id}
                     ItemSeparatorComponent={this.renderSeparator}
+                    onRefresh={this.handleRefresh}
+                    refreshing={refreshing}
                 />
             </View>
         );
