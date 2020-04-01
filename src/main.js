dbf95336af3f1548382ad5a7db0e129deba32f77
Jacob A <jacob@Jacobs-MacBook-Pro.local>
2020-03-31 22:13:32 -0400

    added ngrok

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ const initHttpServer = require('./node/httpServer');
 const { initP2PServer } = require('./node/p2pServer');
 const BlockChain = require('./node/blockChain');
 const os = require('os');
+const ngrok = require('ngrok');
 
 const blockchain = new BlockChain();
 
 initHttpServer(blockchain);
 initP2PServer(blockchain, os.hostname());
+
+(async function() {
+  const url = await ngrok.connect(3001);
+  console.log(url);
+})();
